0b458737bc3bfa758743fd4d1b65b14e715247be
Gurmeet142 <Gamkhilg@gmail.com>
2024-12-19 10:26:15 +0000

    Lebenslauf fast fertig

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,58 @@
 import streamlit as st
 
-st.title("My new app🤡", anchor=False)
-st.header("Ich bin eine neue Überschrift💀", anchor=False, )
-st.subheader("Noch eine kleinere Überschrift🗿", anchor=False, )
-st.write("Das ist meine Streamlit app🍷")
+st.markdown("<style> .stAppHeader {display:none;} ul {list-style-type: none; } </style>", unsafe_allow_html=True)
 
-st.markdown("<p>Ich bin ein Text</p>", unsafe_allow_html=True)
+from pathlib import Path
 
-st.markdown("<a href='https://www.google.at>Link<a/>", unsafe_allow_html=True)
+def get_file_content_as_bytes(file_path):
+    with open(file_path, "rb") as file:
+        return file.read()
+
+# Pfad zur PDF-Datei
+file_path = 'Lebenslauf - konvertiert.pdf'
+
+# Lese den Inhalt der PDF-Datei als Bytes
+file_bytes = get_file_content_as_bytes(file_path)
+
+left,right = st.columns(2)
+
+left.image("profile-pic.png", width=250)
+
+right.markdown("""
+               <h3>Gurmeet GAMKHIL </h3>
+                <em>Ich interessiere mich für KI und wie schnell es sich weiterentwickelt.</em>
+           """, unsafe_allow_html=True)
+
+right.download_button(label="📄 Download.Lebenslauf",
+        data=file_bytes,
+        file_name=file_path,
+        mime='application/pdf')
+
+st.write("📩", "Gamkhilg@gmail.com")
+
+
 st.header("IT-Kompetenzen", anchor=False, divider="blue")
 
 st.write("""
          - 💻 Webentwicklung: Fundierte Grundkenntnisse in HTML, CSS und Streamlit (Fullstack-Framework) 
-         - Programmierung: Praktische Erfahrung in Python, Entwicklung kleiner Anwendungen und Skripte
-         - Office-Suite: Versierter Umgang mit Microsoft Word, Excel und PowerPoint
-        - Eigene Projekte: Konzeption und Umsetzung verschiedener Projekte inklusive Hosting
-         - Schulprojekte: Erstellung datenbasierter Präsentationen und interaktiver Tabellenkalkulationen            
+         - 📱 Programmierung: Praktische Erfahrung in Python, Entwicklung kleiner Anwendungen und Skripte
+         - 🏢 Office-Suite: Versierter Umgang mit Microsoft Word, und PowerPoint
+        - 📽️ Eigene Projekte: Konzeption und Umsetzung verschiedener Projekte inklusive Hosting           
             """, unsafe_allow_html=True)
 
 st.header("Arbeitserfahrung", anchor=False, divider="blue")
-st.write("Berufspraktische Tage 1: Bei XYZ von 18. bis 22. Nov. 2024")
-st.write("Berufspraktische Tage 2: Bei XYZ von 24. bis 28. Feb. 2025")
+st.write(" 💼 Berufspraktische Tage 1: Bei Namaste Store von 15. bis 19. Jän. 2024")
+st.write(" 💼 Berufspraktische Tage 2: Bei Namaste Store von 18. bis 22. Nov. 2024")
 
 
 st.header("Zusätzliche Qualifikationen", anchor=False, divider="blue")
-st.write("Schnelle Auffassungsgabe für neue Softwareanwendung und Technologien")
-st.write("Große Interesse an der kontinuierlichen Weiterentwicklung im IT-Bereich")
-st.write("Teamfähigkeit und Kommunikationsstärke bei gemeinsamen Coding-Projekten")
+st.write(" 🤓 Schnelle Auffassungsgabe für neue Softwareanwendung und Technologien")
+st.write(" 😲 Große Interesse an der kontinuierlichen Weiterentwicklung im IT-Bereich")
+st.write(" 🥇 Teamfähigkeit und Kommunikationsstärke bei gemeinsamen Coding-Projekten")
+st.write(" 🤵‍♂️ Höfflickeit entgegen Kunden und Mitarbeiter")
+st.write(" 🎯 Auf die Genauigkeit achten")
 
 st.header("Interessen und Hobbys",anchor=False, divider="blue")
-st.write("Fußball:Mitglied in einem Fußball-Klub")
-st.write("Lesen: Begeisterte Leser verschiedenster Literatur")
-st.write("Schach: Engagiert im Schachklub")
+st.write("🖥️ Computerspiele spielen")
+st.write(" 🙂 Neue Erfahrungen erleben")
+st.write(" 😀 Etwas neues über das Computertechnik lernen")
